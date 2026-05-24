fbd3a40ee3e92e894a2295e4a7a1c6ceaad07cf5
k3mystra <dev@k3mystra.cc>
2026-05-24 09:37:24 +0800

    feat: Separate out WindowCallbackData

---
+++ src/WindowCallbackData.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct WindowCallbackData {
+    float targetAspectRatio;
+    int viewportX;
+    int viewportY;
+    int viewportWidth;
+    int viewportHeight;
+    float deltaTime;
+};
