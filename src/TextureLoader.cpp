d2a72ec97a1b693ea3073c9dbff4cd371d42bcfa
k3mystra <dev@k3mystra.cc>
2026-05-25 09:07:26 +0800

    fix: Split TextureLoader file

---
+++ src/TextureLoader.cpp
@@ -0,0 +1,31 @@
+#include "TextureLoader.h"
+
+unsigned int loadTexture(const std::string& path) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nChannels;
+    stbi_set_flip_vertically_on_load(true); // OpenGL expects flipped Y
+    unsigned char* data = stbi_load(path.c_str(), &width, &height, &nChannels, 0);
+
+    if (data) {
+        GLenum format = (nChannels == 4) ? GL_RGBA : GL_RGB;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        std::cout << "Loaded texture: " << path << " (" << width << "x" << height << " ch:" << nChannels << ")\n";
+    } else {
+        std::cerr << "Failed to load texture: " << path << "\n";
+        std::cerr << "Reason: " << stbi_failure_reason() << "\n";
+    }
+
+    stbi_image_free(data);
+    return textureID;
+}
