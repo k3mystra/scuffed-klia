5b26909a8cd6f7e28911ceac2f7813b9ba5a0790
k3mystra <dev@k3mystra.cc>
2026-07-13 22:19:29 +0800

    feat: Remove unneeded files

--- src/AmbientLight.cpp
+++
@@ -1,6 +0,0 @@
-#include "AmbientLight.h"
-
-AmbientLight::AmbientLight() : Light() {}
-
-AmbientLight::AmbientLight(const glm::vec3& color)
-    : Light(color) {}
--- src/AmbientLight.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Light.h"
-
-class AmbientLight : public Light {
-public:
-  AmbientLight();
-  AmbientLight(const glm::vec3& color);
-};
