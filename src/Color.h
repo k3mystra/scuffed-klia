2636d239b927b15668e0e54d4ec53609dcda8f4e
k3mystra <dev@k3mystra.cc>
2026-07-14 12:10:02 +0800

    feat: Add color

---
+++ src/Color.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace COLOR {
+    constexpr glm::vec3 RED   = glm::vec3(1.0f, 0.0f, 0.0f);
+    constexpr glm::vec3 GREEN = glm::vec3(0.0f, 1.0f, 0.0f);
+    constexpr glm::vec3 BLUE  = glm::vec3(0.0f, 0.0f, 1.0f);
+
+    constexpr glm::vec3 YELLOW  = glm::vec3(1.0f, 1.0f, 0.0f);
+    constexpr glm::vec3 CYAN    = glm::vec3(0.0f, 1.0f, 1.0f);
+    constexpr glm::vec3 MAGENTA = glm::vec3(1.0f, 0.0f, 1.0f);
+
+    constexpr glm::vec3 WHITE = glm::vec3(1.0f, 1.0f, 1.0f);
+    constexpr glm::vec3 BLACK = glm::vec3(0.0f, 0.0f, 0.0f);
+    constexpr glm::vec3 GREY = glm::vec3(0.5f, 0.5f, 0.5f);
+}
