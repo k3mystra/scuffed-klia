915551d075843ea9e2b187e0686bac0c7076054c
k3mystra <dev@k3mystra.cc>
2026-07-14 14:06:20 +0800

    feat: new exporting shit

--- src/3DScene/LarpCombat/SceneExporter.gd
+++ src/3DScene/LarpCombat/SceneExporter.gd
@@ -2,10 +2,15 @@
 extends EditorScript
 
 # Set the path where your C++ engine expects to read the layout configuration
-const OUTPUT_PATH = "res://scene_layout.txt"
+const OUTPUT_PATH = "res://world.txt"
+
+const ENTITY_DATA_PREFIX = "#"
+const TRANSFORM_PREFIX = "T"
+const MODEL_PREFIX = "M"
+const CAMERA_PREFIX = "C"
 
 func _run():
-	var root = get_editor_interface().get_edited_scene_root()
+	var root = EditorInterface.get_edited_scene_root()
 	if not root:
 		print("Error: Open a scene first!")
 		return
@@ -16,30 +21,70 @@ func _run():
 		return
 		
 	print("Starting export for scene: ", root.name)
-	parse_node(root, file)
+	parse_entities(root, file)
 	file.close()
 	print("Export complete! Saved to: ", OUTPUT_PATH)
 
-func parse_node(node: Node, file: FileAccess):
+
+func parse_entities(node: Node, file: FileAccess):
+	var entityStr = "";
 	if node is MeshInstance3D:
-		if node.mesh and node.mesh.resource_path.get_extension() == "obj":
-			# 1. Get the local clean path to the obj asset
-			var obj_path = node.mesh.resource_path.trim_prefix("res://")
-			obj_path = "3DScene/LarpCombat/" + obj_path
-			# 2. Extract Transform properties
-			var pos = node.position
-			var rot = node.rotation
-			var scl = node.scale
-			
-			# 3. Format line: path pos.x pos.y pos.z rot.x rot.y rot.z scl.x scl.y scl.z
-			var line = "%s %f %f %f %f %f %f %f %f %f\n" % [
-				obj_path,
-				pos.x, pos.y, pos.z,
-				rad_to_deg(rot.x), rad_to_deg(rot.y), rad_to_deg(rot.z), # Converts radians to degrees!
-				scl.x, scl.y, scl.z
-			]
-			file.store_string(line)
+		entityStr = parse_3d_obj(node)
+	elif node is Camera3D:
+		entityStr = parse_cam(node)
+
+	file.store_string(entityStr)
 			
 	# Recursively check children to support nested nodes/groups
 	for child in node.get_children():
-		parse_node(child, file)
+		parse_entities(child, file)
+
+
+func parse_3d_obj(node: MeshInstance3D) -> String:
+	var entityStr = "";
+	entityStr += parse_entity_data_comp(node);
+	entityStr += parse_transform_comp(node);
+	entityStr += parse_model_comp(node);
+
+	return entityStr + "\n";
+
+
+func parse_cam(node: Camera3D):
+	var entityStr = "";
+	entityStr += parse_entity_data_comp(node);
+	entityStr += parse_transform_comp(node);
+	entityStr += parse_camera_comp(node);
+
+	return entityStr;
+
+
+func parse_transform_comp(node: Node3D) -> String:
+	var pos = node.position
+	var rot = node.quaternion
+	var scl = node.scale
+	
+	# 3. Format line: path pos.x pos.y pos.z rot.w rot.x rot.y rot.z scl.x scl.y scl.z
+	var line = TRANSFORM_PREFIX + " "
+	line += "%f %f %f " % [pos.x, pos.y, pos.z]
+	line += "%f %f %f %f " % [rot.w, rot.x, rot.y, rot.z]
+	line += "%f %f %f\n" % [scl.x, scl.y, scl.z]
+
+	return line;
+
+
+func parse_entity_data_comp(node: Node) -> String:
+	return ENTITY_DATA_PREFIX + " " + node.name + "\n"
+
+
+func parse_model_comp(node: MeshInstance3D) -> String:
+	var line = MODEL_PREFIX
+
+	if not node.mesh or node.mesh.resource_path.get_extension() != "obj":
+		return line + "\n";
+
+	line += " " + ProjectSettings.globalize_path(node.mesh.resource_path) + "\n"
+	return line;
+
+
+func parse_camera_comp(node: Camera3D) -> String:
+	return CAMERA_PREFIX + "\n"
