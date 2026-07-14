5ab4aa7e9347d5b2e7113c2d87d4af5edf26f052
k3mystra <dev@k3mystra.cc>
2026-07-14 12:11:29 +0800

    feat: Put defintion into transform_utils

--- src/Transform.cpp
+++ src/Transform.cpp
@@ -1,25 +1,24 @@
 #include "Transform.h"
 
-#include <string>
-
 #include <glm/gtx/quaternion.hpp>
 #include <glm/gtc/quaternion.hpp>
 
+namespace transform_utils {
+    void recalcTransform(Transform& transform) {
+        transform.matrix = glm::mat4(1);
 
-void recalcTransform(Transform& transform) {
-    transform.matrix = glm::mat4(1);
+        transform.matrix = glm::translate(transform.matrix, transform.position);
+        transform.matrix = transform.matrix * glm::toMat4(transform.rotation);
+        transform.matrix = glm::scale(transform.matrix, transform.scale);
 
-    transform.matrix = glm::translate(transform.matrix, transform.position);
-    transform.matrix = transform.matrix * glm::toMat4(transform.rotation);
-    transform.matrix = glm::scale(transform.matrix, transform.scale);
+        transform.invMatrix = glm::inverse(transform.matrix);
 
-    transform.invMatrix = glm::inverse(transform.matrix);
-
-    transform.isDirty = false;
+        transform.isDirty = false;
+    }
 }
 
 void updateTransform(World& world) {
     for (Transform& transform : world.transformList) {
-        recalcTransform(transform);
+        transform_utils::recalcTransform(transform);
     }
 }
