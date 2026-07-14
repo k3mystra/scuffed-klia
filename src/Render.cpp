1410adc523457d170742502ff909bc9c0420bbce
k3mystra <dev@k3mystra.cc>
2026-07-14 17:26:38 +0800

    feat: Add OpenGL debugging stuff

--- src/Render.cpp
+++ src/Render.cpp
@@ -5,6 +5,7 @@
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <string>
 #include <utility>
 
 #include "Render.h"
@@ -26,6 +27,104 @@ const glm::vec3 DEFAULT_BG = COLOR::GREY;
 typedef std::pair<Camera, Transform> CameraTransformData;
 typedef std::pair<Model, Transform> ModelTransformData;
 
+
+void APIENTRY openGLDebugCallback(GLenum source, GLenum type, GLuint id,
+                            GLenum severity, GLsizei length,
+                            const GLchar *msg, const void *data)
+{
+    std::string _source;
+    std::string _type;
+    std::string _severity;
+
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:
+        _source = "API";
+        break;
+
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        _source = "WINDOW SYSTEM";
+        break;
+
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        _source = "SHADER COMPILER";
+        break;
+
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+        _source = "THIRD PARTY";
+        break;
+
+        case GL_DEBUG_SOURCE_APPLICATION:
+        _source = "APPLICATION";
+        break;
+
+        case GL_DEBUG_SOURCE_OTHER:
+        _source = "UNKNOWN";
+        break;
+
+        default:
+        _source = "UNKNOWN";
+        break;
+    }
+
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+        _type = "ERROR";
+        break;
+
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        _type = "DEPRECATED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        _type = "UDEFINED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_PORTABILITY:
+        _type = "PORTABILITY";
+        break;
+
+        case GL_DEBUG_TYPE_PERFORMANCE:
+        _type = "PERFORMANCE";
+        break;
+
+        case GL_DEBUG_TYPE_OTHER:
+        _type = "OTHER";
+        break;
+
+        case GL_DEBUG_TYPE_MARKER:
+        _type = "MARKER";
+        break;
+
+        default:
+        _type = "UNKNOWN";
+        break;
+    }
+
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+        _severity = "HIGH";
+        break;
+
+        case GL_DEBUG_SEVERITY_MEDIUM:
+        _severity = "MEDIUM";
+        break;
+
+        case GL_DEBUG_SEVERITY_LOW:
+        _severity = "LOW";
+        break;
+
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+        _severity = "NOTIFICATION";
+        break;
+
+        default:
+        _severity = "UNKNOWN";
+        break;
+    }
+
+    printf("%d: %s of %s severity, raised from %s: %s\n",
+            id, _type.c_str(), _severity.c_str(), _source.c_str(), msg);
+}
 
 static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
     WindowCallbackData *data = (WindowCallbackData*)glfwGetWindowUserPointer(window);
@@ -165,6 +264,12 @@ void RenderSystem::renderSystemInit(World& world, unsigned int initialWindowWidt
     // OpenGL Functions to enable
     glEnable(GL_DEPTH_TEST);
 
+    #ifdef ENABLE_OPENGL_DEBUG
+    // Debugging
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(openGLDebugCallback, 0);
+    #endif
+
     initializeComponents(world);
 }
 
@@ -194,9 +299,9 @@ void RenderSystem::resetBuffer() {
         windowCallbackData.viewportHeight
     );
     glClearColor(
-        DEFAULT_BG.x,
-        DEFAULT_BG.y,
-        DEFAULT_BG.z,
+        DEFAULT_BG.r,
+        DEFAULT_BG.g,
+        DEFAULT_BG.b,
         1.0f
     );
     glClear(GL_COLOR_BUFFER_BIT);
@@ -278,8 +383,4 @@ void RenderSystem::render(World& world) {
 
         renderObj(modelObjectData, camObjectData, world);
     }
-
-    // Bunch of window handling
-    glfwSwapBuffers(window);
-    glfwPollEvents();    
 }
